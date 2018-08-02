9608380c840e035929ff39f7493234c59e21df2e
flowkater <chjw102@gmail.com>
2018-08-02 15:18:42 +0900

    complete Movieql sample client

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,10 @@
-import React from 'react';
+
+import React from "react";
 import styled from "styled-components";
-import Movie from "./Movie";
 import { Query } from "react-apollo";
-import { HOME_PAGE } from './queries';
+import { Helmet } from "react-helmet";
+import { HOME_PAGE } from "./queries";
+import Movie from "./Movie";
 
 const Container = styled.div`
   display: grid;
@@ -13,21 +15,22 @@ const Container = styled.div`
 
 const Home = () => (
   <Container>
+    <Helmet>
+      <title>Home | MovieQL</title>
+    </Helmet>
     <Query query={HOME_PAGE}>
       {({ loading, data, error }) => {
         if (loading) return "loading";
         if (error) return "something happened";
-        if (data) {
-          return data.movies.map(movie => (
-            <Movie 
-              id={movie.id}
-              key={movie.id}
-              poster={movie.medium_cover_image}
-              title={movie.title}
-              rating={movie.rating}
-            />
-          ));
-        }
+        return data.movies.map(movie => (
+          <Movie
+            id={movie.id}
+            key={movie.id}
+            poster={movie.medium_cover_image}
+            title={movie.title}
+            rating={movie.rating}
+          />
+        ));
       }}
     </Query>
   </Container>
